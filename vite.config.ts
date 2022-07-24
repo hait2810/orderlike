7c3f55f95fcc431fe9ab31ce31eef9e1ace9b7dd
Hai <checked.kenvip10@gmail.com>
2022-07-24 15:04:13 +0700

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+/**
+  * https://vitejs.dev/config/
+  * @type {import('vite').UserConfig}
+  */
+export default {
+   plugins: [vue()],
+
+   // add this:
+   server: { 
+     host: '0.0.0.0'
+   } 
+}
